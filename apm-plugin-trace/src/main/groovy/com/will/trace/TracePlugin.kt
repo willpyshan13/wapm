2fbba3fdd492e7798c72c87defed7e1fbad08c1d
pengyushan <pengyushan@vv.cn>
2019-09-26 17:59:31 +0800

    1、提交代码

---
+++ apm-plugin-trace/src/main/groovy/com/will/trace/TracePlugin.kt
@@ -0,0 +1,22 @@
+package com.will.trace
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Desc:trace plugin
+ *
+ * Date: 2019-09-26 17:13
+ * Copyright: Copyright (c) 2018-2019
+ * Company:
+ * Updater:
+ * Update Time:
+ * Update Comments:
+ * @author: pengysh
+ */
+class TracePlugin : Plugin<Project> {
+    override fun apply(p0: Project) {
+
+    }
+
+}
